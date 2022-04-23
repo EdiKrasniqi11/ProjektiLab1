86616225d0637dc8f568a4644d6e36350fab6208
rreze rexhepi <rr51155@ubt-uni.net>
2022-04-24 00:33:45 +0200

    Shteti-Dashboard_Pt2

--- react-code/src/Shtetet/Shtetet.js
+++ react-code/src/Shtetet/Shtetet.js
@@ -9,7 +9,10 @@ export class Shtetet extends Component{
         super(props);
         
         this.state={
-            shtete:[]
+            shtete:[],
+            modalTitle:"",
+            Emri:"",
+            ShtetiID:0
         }
     }
     refreshList(){
@@ -22,9 +25,37 @@ export class Shtetet extends Component{
     componentDidMount(){
         this.refreshList();
     }
+    changeEmri = (e)=>{
+        this.setState({Emri:e.target.value});
+    }
+    createClick(){
+        fetch(variables.API_URL+'shteti',{
+            method:'POST',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                Emri:this.state.Emri
+            })
+        })
+            .then(res=>res.json())
+            .then((result)=>{
+                alert(result);
+                this.refreshList();
+            },(error)=>{
+                alert('Failed');
+            
+        })
+    }
 
         render(){
-            const{shtete}=this.state;
+            const{
+                shtete,
+                modalTitle,
+                ShtetiID,
+                Emri
+            }=this.state;
 
     return(
         <div id={ShtetiCSS.shtetetDiv}>
@@ -54,6 +85,17 @@ export class Shtetet extends Component{
                     </tr>
                     )}</tbody>
                     </table>
+                    <div id={ShtetiCSS.formShteti}>
+                        <span id={ShtetiCSS.inputText}>Shteti</span>
+                      <div id={ShtetiCSS.inputShteti}><input type="text" id={ShtetiCSS.emriShtetit} value={Emri} onChange={this.changeEmri}/></div>
+                        {ShtetiID ==0?
+                        <button type="button" id={ShtetiCSS.button1} onClick={()=>this.createClick()}>Create</button>
+                        :null}
+                        {ShtetiID !=0?
+                        <button type="button" id={ShtetiCSS.button1}>Update</button>
+                        :null}
+
+                    </div>
         </div>
     )
 }
