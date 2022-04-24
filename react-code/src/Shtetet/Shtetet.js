60aa028eaa7a00f95015de4f0c590515f3e2bc26
rreze rexhepi <rr51155@ubt-uni.net>
2022-04-24 22:59:12 +0200

    ShtetiCrud-FrontEnd

--- react-code/src/Shtetet/Shtetet.js
+++ react-code/src/Shtetet/Shtetet.js
@@ -48,6 +48,53 @@ export class Shtetet extends Component{
             
         })
     }
+    deleteClick(id){
+        if(window.confirm('Are you sure?')){
+        fetch(variables.API_URL+'shteti/'+id,{
+            method:'DELETE',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            }
+        })
+            .then(res=>res.json())
+            .then((result)=>{
+                alert(result);
+                this.refreshList();
+            },(error)=>{
+                alert('Failed');
+            
+        })
+    }
+    }
+    updateClick(){
+        fetch(variables.API_URL+'shteti',{
+            method:'PUT',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                ShtetiID:this.state.ShtetiID,
+                Emri:this.state.Emri
+            })
+        })
+            .then(res=>res.json())
+            .then((result)=>{
+                alert(result);
+                this.refreshList();
+            },(error)=>{
+                alert('Failed');
+            
+        })
+    }
+    editClick(shtetet){
+        this.setState({
+            modalTitle:"Edit Emri",
+            ShtetiID:shtetet.ShtetiID,
+            Emri:shtetet.Emri
+        });
+    }
 
         render(){
             const{
@@ -79,12 +126,29 @@ export class Shtetet extends Component{
                         <td>{shtetet.ShtetiID}</td>
                         <td>{shtetet.Emri}</td>
                         <td>
-                            <button type="button" id={ShtetiCSS.button1}>Edit</button>
-                            <button type="button" id={ShtetiCSS.button1}>Delete</button>
+                            <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={()=>this.editClick(shtetet)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+</svg></button>
+                            <button type="button" onClick={()=>this.deleteClick(shtetet.ShtetiID)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg></button>
                         </td>
                     </tr>
                     )}</tbody>
                     </table>
+
+                    <div className="modal fade" id="exampleModal" tabIndex="-1" aria-hidden="true">
+                        <div className="modal-dialog modal-lg modal-dialog-centered">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title">{modalTitle}</h5>
+                                    <button type="button" id={ShtetiCSS.button} data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                          
+                            </div>
+                        </div>
+                    </div>
                     <div id={ShtetiCSS.formShteti}>
                         <span id={ShtetiCSS.inputText}>Shteti</span>
                       <div id={ShtetiCSS.inputShteti}><input type="text" id={ShtetiCSS.emriShtetit} value={Emri} onChange={this.changeEmri}/></div>
@@ -92,7 +156,7 @@ export class Shtetet extends Component{
                         <button type="button" id={ShtetiCSS.button1} onClick={()=>this.createClick()}>Create</button>
                         :null}
                         {ShtetiID !=0?
-                        <button type="button" id={ShtetiCSS.button1}>Update</button>
+                        <button type="button" id={ShtetiCSS.button1} onClick={()=>this.updateClick()}>Update</button>
                         :null}
 
                     </div>
