1ed415c91f48357e7b24246e875afe3ee54564fd
rreze rexhepi <rr51155@ubt-uni.net>
2022-04-30 16:37:01 +0200

    Qyteti FrontEnd Final

--- MSSQL/LaboratoriDatabase.sql
+++ MSSQL/LaboratoriDatabase.sql
@@ -22,8 +22,9 @@ create table Shteti(
  
  create table Qyteti(
 	QytetiID int identity(1,1) primary key,
-	Shteti varchar(255) not null,
-	Emri varchar(255) not null
+	Emri varchar(255) not null,
+	Shteti varchar(255) not null
+	
 )
 
 DBCC CHECKIDENT ('Njoftimet', RESEED, 0);
