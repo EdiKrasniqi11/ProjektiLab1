b06e849acd212903b256d91aa361e2794e32bb2a
Edi Krasniqi <ek51185@ubt-uni.net>
2022-06-13 15:09:17 +0200

    PasswordShown+CSS Tweaks

--- react-code/src/Studenti/Studenti.js
+++ react-code/src/Studenti/Studenti.js
@@ -39,7 +39,8 @@ export class Studenti extends Component{
             Drejtimi:0,
             Specializimi:0,
             insertModal:false,
-            dataModal:false
+            dataModal:false,
+            passwordShown:false
         }
     }
     refreshList(){
@@ -134,65 +135,76 @@ export class Studenti extends Component{
     }
     createClick(){
         var emri = this.state.Emri+" "+this.state.Mbiemri;
-        var ditelindja = this.state.Dita+"/"+this.state.Muaji+"/"+this.state.Viti
-        fetch(variables.API_URL+'studenti',{
-            method:'POST',
-            headers:{
-                'Accept':'application/json',
-                'Content-Type':'application/json'
-            },
-            body:JSON.stringify({
-                Emri:emri,
-                Email:this.state.Email,
-                Password:this.state.Password,
-                Datelindja:ditelindja,
-                Gjinia:this.state.Gjinia,
-                Vendbanimi:this.state.Vendbanimi,
-                Fakulteti:this.state.Fakulteti,
-                Dega:this.state.Dega,
-                Drejtimi:this.state.Drejtimi,
-                Specializimi:this.state.Specializimi
+        var ditelindja = this.state.Dita+"/"+this.state.Muaji+"/"+this.state.Viti;
+        let digitRegex = /[1-9]/;
+        if(!this.state.Email.endsWith('@ubt-uni.net')){
+            return alert("Email must end in @ubt-uni.net");
+        }else{
+            fetch(variables.API_URL+'studenti',{
+                method:'POST',
+                headers:{
+                    'Accept':'application/json',
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({
+                    Emri:emri,
+                    Email:this.state.Email,
+                    Password:this.state.Password,
+                    Datelindja:ditelindja,
+                    Gjinia:this.state.Gjinia,
+                    Vendbanimi:this.state.Vendbanimi,
+                    Fakulteti:this.state.Fakulteti,
+                    Dega:this.state.Dega,
+                    Drejtimi:this.state.Drejtimi,
+                    Specializimi:this.state.Specializimi
+                })
             })
-        })
-            .then(res=>res.json())
-            .then((result)=>{
-                alert(result);
-                this.refreshList();
+                .then(res=>res.json())
+                .then((result)=>{
+                    alert(result);
+                    this.refreshList();
+                })
+            this.setState({
+                insertModal:false,
+                passwordShown:false
             })
-        this.setState({
-            insertModal:false
-        })
+        }
     }
     updateClick(){
         var emri = this.state.Emri+" "+this.state.Mbiemri;
-        var ditelindja = this.state.Dita+"/"+this.state.Muaji+"/"+this.state.Viti
-        fetch(variables.API_URL+'studenti',{
-            method:'PUT',
-            headers:{
-                'Accept':'application/json',
-                'Content-Type':'application/json'
-            },
-            body:JSON.stringify({
-                StudentiID:this.state.StudentiID,
-                Emri:emri,
-                Email:this.state.Email,
-                Password:this.state.Password,
-                Datelindja:ditelindja,
-                Gjinia:this.state.Gjinia,
-                Vendbanimi:this.state.Vendbanimi,
-                Fakulteti:this.state.Fakulteti,
-                Dega:this.state.Dega,
-                Drejtimi:this.state.Drejtimi,
-                Specializimi:this.state.Specializimi
+        var ditelindja = this.state.Dita+"/"+this.state.Muaji+"/"+this.state.Viti;
+        if(!this.state.Email.endsWith('@ubt-uni.net')){
+            return alert("Email must end in @ubt-uni.net");
+        }else{
+            fetch(variables.API_URL+'studenti',{
+                method:'PUT',
+                headers:{
+                    'Accept':'application/json',
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({
+                    StudentiID:this.state.StudentiID,
+                    Emri:emri,
+                    Email:this.state.Email,
+                    Password:this.state.Password,
+                    Datelindja:ditelindja,
+                    Gjinia:this.state.Gjinia,
+                    Vendbanimi:this.state.Vendbanimi,
+                    Fakulteti:this.state.Fakulteti,
+                    Dega:this.state.Dega,
+                    Drejtimi:this.state.Drejtimi,
+                    Specializimi:this.state.Specializimi
+                })
+            }).then(res=>res.json())
+            .then((result)=>{
+                    alert(result);
+                    this.refreshList();
+                })
+            this.setState({
+                insertModal:false,
+                passwordShown:false
             })
-        }).then(res=>res.json())
-        .then((result)=>{
-                alert(result);
-                this.refreshList();
-            })
-        this.setState({
-            insertModal:false
-        })
+        }
     }
     deleteClick(id){
         if(window.confirm('Are you sure?')){
@@ -252,7 +264,8 @@ export class Studenti extends Component{
             Dega:0,
             Drejtimi:0,
             Specializimi:0,
-            insertModal:true
+            insertModal:true,
+            passwordShown:false
         });
     }
     formatDate(date){
@@ -294,7 +307,8 @@ export class Studenti extends Component{
             Drejtimi,
             Specializimi,
             insertModal,
-            dataModal
+            dataModal,
+            passwordShown
         }=this.state;
         let years=[];
         for(let i=2022;i>1957;i--){
@@ -348,7 +362,7 @@ export class Studenti extends Component{
                             <input type="text" value={Email} onChange={this.changeEmail} placeholder="Email"/>
                         </div>
                         <div id={stylist.passwordInputDiv}>
-                            <input type="password" value={Password} onChange={this.changePassword} placeholder="Email"/>
+                            <input type={passwordShown? "text" : "password"} value={Password} onChange={this.changePassword} placeholder="Password" id="passwordID"/><svg id={stylist.showPassword} onClick={() => {if(this.state.passwordShown){this.setState({passwordShown: false})}else{this.setState({passwordShown: true})}}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16  "><path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/><path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/></svg>
                         </div>
                         <div id={stylist.date}>
                             <select id="dayInput" onChange={this.changeDita} value={Dita}>
