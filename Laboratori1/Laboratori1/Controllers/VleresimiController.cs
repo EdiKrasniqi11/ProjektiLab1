300663d894b1100d634fc51bec244713ba781585
adelina xhema <ax52468@ubt-uni.net>
2022-06-16 18:58:50 +0200

    Vleresimi-Backend

---
+++ Laboratori1/Laboratori1/Controllers/VleresimiController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Laboratori1.Objects;
+
+namespace Laboratori1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VleresimiController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public VleresimiController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select * from Vleresimi";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SMISAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(Vleresimi vleresimet)
+        {
+            string query = @"insert into Vleresimi values(@Studenti,@Lenda,@Nota)";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SMISAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Studenti", vleresimet.Studenti);
+                    myCommand.Parameters.AddWithValue("@Lenda", vleresimet.Lenda);
+                    myCommand.Parameters.AddWithValue("@Nota", vleresimet.Nota);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Successful Insertion");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Vleresimi vleresimet)
+        {
+            string query = @"update Vleresimi set Nota = @Nota where Lenda = @Lenda and Studenti = @Studenti";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SMISAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Studenti", vleresimet.Studenti);
+                    myCommand.Parameters.AddWithValue("@Lenda", vleresimet.Lenda);
+                    myCommand.Parameters.AddWithValue("@Nota", vleresimet.Nota);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Successful Update");
+        }
+
+        [HttpDelete("{studenti}/{lenda}")]
+        public JsonResult Delete(int studenti, int lenda)
+        {
+            string query = @"delete from Vleresimi where Lenda = @Lenda and Studenti = @Studenti";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SMISAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Studenti", studenti);
+                    myCommand.Parameters.AddWithValue("@Lenda", lenda);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Successful Deletion");
+        }
+    }
+}
---
+++ Laboratori1/Laboratori1/Objects/Vleresimi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratori1.Objects
+{
+    public class Vleresimi
+    {
+        public int Studenti
+        {
+            get;
+            set;
+        }
+        public int Lenda
+        {
+            get;
+            set;
+        }
+        public int Nota
+        {
+            get;
+            set;
+        }
+
+    }
+}
