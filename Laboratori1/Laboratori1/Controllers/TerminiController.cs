32305cfa945486efc23ee94eb2e4f70248e93f41
adelina xhema <ax52468@ubt-uni.net>
2022-06-17 20:49:35 +0200

    Termini-BackEnd

---
+++ Laboratori1/Laboratori1/Controllers/TerminiController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Laboratori1.Objects;
+
+namespace Laboratori1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TerminiController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public TerminiController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select * from Termini";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SMISAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(Termini terminet)
+        {
+            string query = @"insert into Termini values(@Studenti,@Terminet)";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SMISAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Studenti", terminet.Studenti);
+                    myCommand.Parameters.AddWithValue("@Terminet", terminet.Terminet);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Successful Insertion");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Termini terminet)
+        {
+            string query = @"update Termini set Termini = @Termini where  Studenti = @Studenti";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SMISAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+
+                    myCommand.Parameters.AddWithValue("@Studenti", terminet.Studenti);
+                    myCommand.Parameters.AddWithValue("@Terminet", terminet.Terminet);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Successful Update");
+        }
+
+        [HttpDelete("{studenti}")]
+        public JsonResult Delete(int studenti)
+        {
+            string query = @"delete from Termini where  Studenti = @Studenti";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SMISAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Studenti", studenti);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Successful Deletion");
+        }
+    }
+}
---
+++ Laboratori1/Laboratori1/Objects/Termini.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratori1.Objects
+{
+    public class Termini
+    {
+        public int Studenti
+        {
+            get;
+            set;
+        }
+        public string Terminet
+        {
+            get;
+            set;
+        }
+    }
+}
